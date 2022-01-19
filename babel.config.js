3f97d4075fe12ab64baf7aa732d986db608af390
venzel <eneas.eng@yahoo.com>
2022-01-19 17:29:30 -0300

    fix: babel config

--- babel.config.js
+++ babel.config.js
@@ -13,6 +13,6 @@ module.exports = {
         ],
         'babel-plugin-transform-typescript-metadata',
         ['@babel/plugin-proposal-decorators', { legacy: true }],
-        ['@babel/plugin-proposal-class-properties', { loose: false }],
+        ['@babel/plugin-proposal-class-properties', { loose: true }],
     ],
 };
